17516b8450f8431be1c704908bb8466ef26d6494
valda-z <valdemar.zavadsky@microsoft.com>
2017-09-15 15:33:13 +0200

    initial commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../whoisbot.zip');
+var kuduApi = 'https://whoisbot.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$whoisbot';
+var password = 's96XLvaowdbx9cvkzF4tPt6pQoKhShpb4Tt0ERLlh4MJRwf4R0aq7qPti2as';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('whoisbot publish');
+  } else {
+    console.error('failed to publish whoisbot', err);
+  }
+});
